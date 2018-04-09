197da3385512ee91bae2ee53fdc06c230be743b0
David Brennan <david.brennan@mail.itsligo.ie>
2018-04-09 20:21:05 +0100

    Added Camera class and updated SceneManager.

--- DSAACA/Backgrounds/SceneManager.cs
+++ DSAACA/Backgrounds/SceneManager.cs
@@ -11,7 +11,7 @@ namespace DSAACA.Backgrounds
     class SceneManager : GameComponent
     {
         #region Properties
-
+        public List<Scene> Scenes;
         #endregion
 
         #region Constructor
